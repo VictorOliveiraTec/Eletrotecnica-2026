0402406c6f1ce50f484a399def886a0559fd135f
VICTOR HUGO <156862702+VictorOliveiraTec@users.noreply.github.com>
2026-05-15 22:41:17 -0300

    ATT

--- main.js
+++ main.js
@@ -1,128 +1,178 @@
+const main = document.querySelector('#conteudo');
 
-const main = document.querySelector('#conteudo')
+const paginas = {
+  home: {
+    titulo: 'Bem-vindo ao nosso site da Eletrotécnica 2026!',
+    center: true,
+    cards: [
+      {
+        titulo: 'Matérias',
+        descricao: 'Acesso a algumas matérias já concluídas.',
+        imagem: 'assets/imgs/logo senai.png',
+        pagina: 'materias'
+      },
+      {
+        titulo: 'Normas Técnicas',
+        descricao: 'Acesso às normas técnicas.',
+        imagem: 'assets/imgs/logo senai.png',
+        pagina: 'normas'
+      },
+      {
+        titulo: 'Senai',
+        descricao: 'Links relacionados ao Senai em geral.',
+        imagem: 'assets/imgs/logo senai.png',
+        pagina: 'senai'
+      }
+    ]
+  },
 
-function trocarConteudo() {
+  materias: {
+    titulo: 'Matérias',
+    cards: [
+      {
+        titulo: 'CIRCUITOS ELÉTRICOS I',
+        descricao: 'CIRCUITOS ELÉTRICOS I',
+        link: 'https://drive.google.com/drive/folders/1QYVJJ2oUnPy-72r7n6krxdBaefBOMSDr?usp=sharing'
+      },
+      {
+        titulo: 'SAÚDE, SEGURANÇA DO TRABALHO E MEIO AMBIENTE',
+        descricao: 'SAÚDE, SEGURANÇA DO TRABALHO E MEIO AMBIENTE',
+        link: 'https://drive.google.com/drive/folders/15_ybpuk88k_FoBRCAppBtg8xlxNSmk_m?usp=sharing'
+      },
+      {
+        titulo: 'CIRCUITOS ELÉTRICOS II',
+        descricao: 'CIRCUITOS ELÉTRICOS II',
+        link: 'https://drive.google.com/drive/folders/1liihAWNyUccafC5A9UGfqIIIvUF7jEri?usp=sharing'
+      },
+      {
+        titulo: 'INSTALAÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP',
+        descricao: 'INSTALAÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP',
+        link: 'https://drive.google.com/drive/folders/1_WoBht6ZS1TljuGRZpjCXDZc8pxnq-MY?usp=sharing'
+      },
+      {
+        titulo: 'GESTÃO DA MANUTENÇÃO',
+        descricao: 'GESTÃO DA MANUTENÇÃO',
+        link: 'https://drive.google.com/drive/folders/1fKkCcEbFu-ZHr1NWxFolF0fEJlyFeLZy?usp=sharing'
+      },
+      {
+        titulo: 'MANUTENÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP',
+        descricao: 'MANUTENÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP',
+        link: 'https://drive.google.com/drive/folders/12bIk-fA5nfgvFH3JV6Dz7miGF8tBRtJ6?usp=sharing'
+      }
+    ]
+  },
 
-  main.innerHTML = `
-  
-  <h1>Bem-vindo ao nosso site da Eletrotécnica 2026!</h1>
+  normas: {
+    titulo: 'Normas Técnicas',
+    cards: [
+      {
+        titulo: 'TinkerCad',
+        descricao: 'Base do Senai, informações básicas.',
+        link: 'https://www.tinkercad.com/'
+      },
+      {
+        titulo: 'Site Norma ABNT',
+        descricao: 'Portal da ABNT.',
+        link: 'https://www.abntcolecao.com.br/'
+      },
+      {
+        titulo: 'Woka Instalações Eletricas',
+        descricao: 'Informações sobre instalações elétricas.',
+        link: 'https://www.woka.com.br/instalacoes-eletricas'
+      }
+    ]
+  },
 
-  <div id="base-conteudo-center">
-    <div class="card" id="Materias">
-      <img src="assets/imgs/logo materias.png" alt="Materias" class="img-cards">
-      <h2>Matérias</h2>
-      <p>Acesso a algumas matérias já concluidas.</p>
-      <button onclick="trocarConteudo1()">Acessar</button>
-    </div>
-    <div class="card" id="Normas-Tecnicas">
-      <img src="assets/imgs/normas tecnicas.png" alt="Normas Tecnicas" class="img-cards">
-      <h2>Normas Técnicas</h2>
-      <p>Acesso às normas técnicas.</p>
-      <button onclick="trocarConteudo2()">Acessar</button>     
-    </div>
-    <div class="card" id="Senai">
-      <img src="assets/imgs/logo senai.png" alt="Senai" class="img-cards">
-      <h2>Senai</h2>
-      <p>Links relacionados ao Senai em geral.</p>
-      <button onclick="trocarConteudo3()">Acessar</button>
-    </div>
-  </div>
+  senai: {
+    titulo: 'Senai',
+    cards: [
+      {
+        titulo: 'Portal Senai',
+        descricao: 'Informações básicas do Senai.',
+        link: 'https://meusenai.senai.br/'
+      },
+      {
+        titulo: 'Portal Fiemg',
+        descricao: 'Portal do aluno.',
+        link: 'https://portaldoaluno.fiemg.com.br/'
+      },
+      {
+        titulo: 'Biblioteca Senai',
+        descricao: 'Acesso à biblioteca.',
+        link: 'https://bibliotecasesisenai.fiemg.com.br/'
+      }
+    ]
+  }
+};
 
-  `;
-}
+function renderizarPagina(nomePagina) {
 
-function trocarConteudo1() {
+  const pagina = paginas[nomePagina];
 
-  main.innerHTML = `
-  
-    <h1>Materias</h1>
+  const cardsHTML = pagina.cards.map(card => {
 
-    <div id="base-conteudo" class="">
-        <div class="card-conteudo">
-          <h3>CIRCUITOS ELÉTRICOS I</h3>
-          <p>CIRCUITOS ELÉTRICOS I</p>
-          <button><a href="https://drive.google.com/drive/folders/1QYVJJ2oUnPy-72r7n6krxdBaefBOMSDr?usp=sharing" target="_blank">Acessar</a></button>
-       </div>
-        <div class="card-conteudo">
-          <h3>CIRCUITOS ELÉTRICOS II</h3>
-          <p>CIRCUITOS ELÉTRICOS II</p>
-          <button><a href="https://drive.google.com/drive/folders/1liihAWNyUccafC5A9UGfqIIIvUF7jEri?usp=sharing" target="_blank">Acessar</a></button>
-       </div>
-        <div class="card-conteudo">
-          <h3>SAÚDE, SEGURANÇA DO TRABALHO E MEIO AMBIENTE</h3>
-          <p>SAÚDE, SEGURANÇA DO TRABALHO E MEIO AMBIENTE</p>
-          <button><a href="https://drive.google.com/drive/folders/15_ybpuk88k_FoBRCAppBtg8xlxNSmk_m?usp=sharing" target="_blank">Acessar</a></button>
-       </div>
-       <div class="card-conteudo">
-          <h3>INSTALAÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP</h3>
-          <p>INSTALAÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP</p>
-          <button><a href="https://drive.google.com/drive/folders/1_WoBht6ZS1TljuGRZpjCXDZc8pxnq-MY?usp=sharing" target="_blank">Acessar</a></button>
-       </div>
-       <div class="card-conteudo">
-          <h3>GESTÃO DA MANUTENÇÃO</h3>
-          <p>GESTÃO DA MANUTENÇÃO</p>
-          <button><a href="https://drive.google.com/drive/folders/1fKkCcEbFu-ZHr1NWxFolF0fEJlyFeLZy?usp=sharing" target="_blank">Acessar</a></button>
-       </div>
-       <div class="card-conteudo">
-          <h3>MANUTENÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP</h3>
-          <p>MANUTENÇÕES DE SISTEMAS ELÉTRICOS DE POTÊNCIA - SEP</p>
-          <button><a href="https://drive.google.com/drive/folders/12bIk-fA5nfgvFH3JV6Dz7miGF8tBRtJ6" target="_blank">Acessar</a></button>
+    // CARD DA HOME
+    if (card.pagina) {
+      return `
+        <div class="card">
+          <img src="${card.imagem}" alt="${card.titulo}" class="img-cards">
+
+          <h2>${card.titulo}</h2>
+
+          <p>${card.descricao}</p>
+
+          <button data-page="${card.pagina}">
+            Acessar
+          </button>
         </div>
-    </div>
+      `;
+    }
 
-  `;
-}
+    // CARD NORMAL
+    return `
+      <div class="card-conteudo">
 
-function trocarConteudo2() {
+        <h3>${card.titulo}</h3>
 
-  main.innerHTML = `
-  
-    <h1>Normas Técnicas</h1>
-    
-    <div id="base-conteudo" class="hidden">
-        <div class="card-conteudo">
-          <h3>TinkerCad</h3>
-          <p>Base do Senai, informações basicas.</p>
-          <button><a href="https://www.tinkercad.com/" target="_blank">Acessar</a></button>
-        </div>
-        <div class="card-conteudo">
-          <h3>Site Norma ABNT</h3>
-          <p>Portal Do Aluno | Boletim e demais informações.</p>
-          <button><a href="https://www.abntcolecao.com.br/" target="_blank">Acessar</a></button>
-        </div>
-        <div class="card-conteudo">
-          <h3>Woka Instalações Eletricas</h3>
-          <p>Woka Instalações Eletricas</p>
-          <button><a href="https://woca.ocalev.com.br/projetos" target="_blank">Acessar</a></button>
-        </div>
+        <p>${card.descricao}</p>
+
+        <a href="${card.link}" target="_blank">
+          <button>Acessar</button>
+        </a>
+
       </div>
-
-  `;
-}
-
-function trocarConteudo3() {
+    `;
+  }).join('');
 
   main.innerHTML = `
   
-  <h1>Senai</h1>
-    
-    <div id="base-conteudo" class="hidden">
-          <div class="card-conteudo">
-            <h3>Portal Senai</h3>
-            <p>Base do Senai, informações basicas.</p>
-            <button><a href="https://meusenai.senai.br/" target="_blank">Acessar</a></button>
-          </div>
-          <div class="card-conteudo">
-            <h3>Portal Fiemg</h3>
-            <p>Portal Do Aluno | Boletim e demais informações.</p>
-            <button><a href="https://portaldoaluno.fiemg.com.br/" target="_blank">Acessar</a></button>
-          </div>
-          <div class="card-conteudo">
-            <h3>Biblioteca Senai</h3>
-            <p>Acesso a Biblioteca Senai.</p>
-            <button><a href="https://bibliotecasesisenai.fiemg.com.br/" target="_blank">Acessar</a></button>
-          </div>
-      </div>
+    <h2>${pagina.titulo}</h2>
+
+    <div 
+      id="${pagina.center ? 'base-conteudo-center' : 'base-conteudo'}"
+    >
+      ${cardsHTML}
+    </div>
 
   `;
+
+  adicionarEventos();
+}
+
+function adicionarEventos() {
+
+  const botoes = document.querySelectorAll('[data-page]');
+
+  botoes.forEach(botao => {
+
+    botao.addEventListener('click', () => {
+
+      const pagina = botao.dataset.page;
+
+      renderizarPagina(pagina);
+
+    });
+
+  });
+
 }
